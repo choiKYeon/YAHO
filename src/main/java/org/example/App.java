e2f3ac750d128570275ecbd8689b6c1381351365
ckhckh2 <gusrudchl12@gmail.com>
2023-10-28 16:12:57 +0900

    10-28 yaho

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,15 @@
 package org.example;
 
-import org.example.member.controller.MemberController;
-
 public class App {
     MemberController memberController = new MemberController();
     public void run(){
+
         System.out.println("○●○● YAHO에 오신걸 환영합니다. ●○●○");
+
         while (true){
-            System.out.println("== 로그인을 진행해주세요. (계정이 없다면 회원가입을 진행해주세요.) ==");
+
+            System.out.println("\n== 로그인을 진행해주세요. (계정이 없다면 회원가입을 진행해주세요.) ==");
+            System.out.println("\n1.로그인 / 2.회원가입 / 3.종료");
             System.out.print("입력 :");
             String command = Container.getSc().nextLine().trim();
 
