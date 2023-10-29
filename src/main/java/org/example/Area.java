e6c455765f6d9cbc4b9616eeba2236367d846e74
ckhckh2 <gusrudchl12@gmail.com>
2023-10-29 15:22:02 +0900

    10-29 yaho

--- src/main/java/org/example/Area.java
+++ src/main/java/org/example/Area.java
@@ -4,30 +4,38 @@ import org.example.Container;
 import org.example.DefaultCommand;
 
 public class Area {
+    MoiuntainController moiuntainController = new MoiuntainController();
     // 지역을 선택하는 메인화면
     public void areaSelcet(){
-        System.out.println("\n== 지역을 선택해주세요. ==");
-        System.out.println("\n1.경기도 / 2.강원도 / 3.충청도 / 4.전라도 / 5.경상도 / 6.제주도");
-        System.out.print("입력 :");
-        String commandArea = Container.getSc().nextLine().trim();
+
         while (true) {
+            System.out.println("\n== 지역을 선택해주세요. ==");
+            System.out.println("\n1.경기도 / 2.강원도 / 3.충청도 / 4.전라도 / 5.경상도 / 6.제주도");
+            System.out.print("입력 :");
+            String commandArea = Container.getSc().nextLine().trim();
 
             switch (commandArea) {
                 case "경기도":
+                    moiuntainController.gyeonggiDo();
                     break;
                 case "강원도":
+                    moiuntainController.gangwonDo();
                     break;
                 case "충청도":
+                    moiuntainController.chungcheongDo();
                     break;
                 case "전라도":
+                    moiuntainController.jeollaDo();
                     break;
                 case "경상도":
+                    moiuntainController.kyongsangDo();
                     break;
                 case "제주도":
+                    moiuntainController.jejuDo();
                     break;
                 default:
                     DefaultCommand.DefaultCommand();
-                    return;
+                    continue;
             }
             break;
         }
--- src/main/java/org/example/MoiuntainController.java
+++ src/main/java/org/example/MoiuntainController.java
@@ -1,7 +1,133 @@
 package org.example;
 
 public class MoiuntainController {
-    public void mountainSeclect(){
-        System.out.println("\n== 산을 선택해주세요. ==");
+    public void gyeonggiDo() {
+        while (true) {
+            System.out.println("\n== 경기도 지역의 산을 선택해주세요. ==");
+            String commandMountain = Container.getSc().nextLine().trim();
+
+            switch (commandMountain) {
+                case "청계산":
+                    break;
+                case "불장산":
+                    break;
+                case "구룡산":
+                    break;
+                case "관악산":
+                    break;
+                default:
+                    DefaultCommand.DefaultCommand();
+                    continue;
+            }
+            break;
+        }
+    }
+    public void gangwonDo() {
+        while (true) {
+            System.out.println("\n== 강원도 지역의 산을 선택해주세요. ==");
+            String commandMountain = Container.getSc().nextLine().trim();
+
+            switch (commandMountain) {
+                case "오대산":
+                    break;
+                case "봉평산":
+                    break;
+                case "설악산":
+                    break;
+                case "태백산":
+                    break;
+                case "속리산":
+                    break;
+                default:
+                    DefaultCommand.DefaultCommand();
+                    continue;
+            }
+            break;
+        }
+    }
+
+    public void chungcheongDo() {
+        while (true) {
+            System.out.println("\n== 충청도 지역의 산을 선택해주세요. ==");
+            String commandMountain = Container.getSc().nextLine().trim();
+
+            switch (commandMountain) {
+                case "계룡산":
+                    break;
+                case "백운산":
+                    break;
+                case "대덕산":
+                    break;
+                case "내장산":
+                    break;
+                case "소백산":
+                    break;
+                default:
+                    DefaultCommand.DefaultCommand();
+                    continue;
+            }
+            break;
+        }
+    }
+    public void kyongsangDo() {
+        while (true) {
+            System.out.println("\n== 경상도 지역의 산을 선택해주세요. ==");
+            String commandMountain = Container.getSc().nextLine().trim();
+
+            switch (commandMountain) {
+                case "공주산":
+                    break;
+                case "토함산":
+                    break;
+                case "대왕약산":
+                    break;
+                case "구룡산":
+                    break;
+                case "콘솔산":
+                    break;
+                default:
+                    DefaultCommand.DefaultCommand();
+                    continue;
+            }
+            break;
+        }
+    }
+    public void jeollaDo() {
+        while (true) {
+            System.out.println("\n== 전라도 지역의 산을 선택해주세요. ==");
+            String commandMountain = Container.getSc().nextLine().trim();
+
+            switch (commandMountain) {
+                case "월출산":
+                    break;
+                case "무등산":
+                    break;
+                case "무안고덕산":
+                    break;
+                case "장안산":
+                    break;
+                case "오리유산":
+                    break;
+                default:
+                    DefaultCommand.DefaultCommand();
+                    continue;
+            }
+            break;
+        }
+    }
+    public void jejuDo() {
+        while (true) {
+            System.out.println("\n== 제주도 지역의 산을 선택해주세요. ==");
+            String commandMountain = Container.getSc().nextLine().trim();
+
+            switch (commandMountain) {
+                case "한라산":
+                    break;
+                default:
+                    DefaultCommand.DefaultCommand();
+                    continue;
+            }
+            break;
+        }
     }
 }
